5bb9e462b47b103540541813683e76223d472315
guillaume leclercq <guillaume.leclercq@fullsix.com>
2017-01-26 13:57:24 +0100

    symfony project and api list + detail view

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# peaks
-technical test
+my_project
+==========
+
+A Symfony project created on January 23, 2017, 3:08 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $publicKey = $this->getParameter('marvel_api_public_key');
+        $privateKey = $this->getParameter('marvel_api_private_key');
+        $client   = $this->get('guzzle.client.marvel');
+
+        $hash = md5('1'.$privateKey.$publicKey);
+
+
+        $response = $client->get('/v1/public/characters', [
+            'query' => [
+                'apikey' => '0d7e9cdff2b90438536e71566a4e0187',
+                'hash'   => $hash,
+                'ts'     => 1,
+                'limit'  => 22,
+                'offset' => 100
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $result['data']['results'],
+            $request->query->getInt('page', 1),
+            6
+        );
+
+        return $this->render('default/index.html.twig', [
+            'pagination' => $pagination,
+        ]);
+    }
+
+    /**
+     * @Route("/character/show/{id}", name="show_character")
+     */
+    public function characterAction(Request $request, $id)
+    {
+        $publicKey = $this->getParameter('marvel_api_public_key');
+        $privateKey = $this->getParameter('marvel_api_private_key');
+        $client   = $this->get('guzzle.client.marvel');
+
+        $hash = md5('1'.$privateKey.$publicKey);
+
+
+        $response = $client->get('/v1/public/characters/'.$id, [
+            'query' => [
+                'apikey' => '0d7e9cdff2b90438536e71566a4e0187',
+                'hash'   => $hash,
+                'ts'     => 1,
+                'limit'  => 1
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+//        $paginator  = $this->get('knp_paginator');
+//        $pagination = $paginator->paginate(
+//            $result['data']['results'],
+//            $request->query->getInt('page', 1),
+//            6
+//        );
+
+        return $this->render('character/show.html.twig', [
+            'character' => $result['data']['results'][0],
+        ]);
+    }
+}
